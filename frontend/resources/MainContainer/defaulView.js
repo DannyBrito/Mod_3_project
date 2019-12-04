a487f921db8a43ebad77572ff5a33c85494352bb
DannyBrito <britodanny1997@gmail.com>
2019-12-04 12:17:19 -0500

    main container - div bar

---
+++ frontend/resources/MainContainer/defaulView.js
@@ -0,0 +1,21 @@
+//main_container
+
+function mainContainerWelcome(){
+    main_container.innerHTML = "<div id='mctitle'>WELCOME</div>"
+    main_container.style.background = "blue"
+}
+
+function LoginMainContainer(){
+    main_container. innerHTML= ""
+    main_container.innerHTML +=
+    `<div class="mc-nav" id="mc-nav-div">
+        <div id="wd-btn"class="mc-nav-item" data-user-id=${currentUser}>wardrobe</div>
+        <div id="current-btn"class="mc-nav-item" data-user-id=${currentUser}>current</div>
+        <div id="editwd-btn"class="mc-nav-item" data-user-id=${currentUser}>edit wardrobe</div>
+    </div>
+    <div id="mc-content-space">
+    </div>`
+    mc_nav_bar = document.getElementById('mc-nav-div')
+    mcContent = document.getElementById('mc-content-space')
+}
+
--- frontend/resources/MainContainer/nav_inter.js
+++ frontend/resources/MainContainer/nav_inter.js
@@ -1,17 +1,19 @@
-const BASE_URL = 'http://localhost:3000/api/v1/users'
+//usersUrl = 'http://localhost:3000/api/v1/users' --- REFERENCE
+
 //get clothing for a user
-function getUserClothing(id){
-    fetch(BASE_URL + `/${id}`)
+function getUserClothing(){
+    fetch(usersUrl + `/${currentUser}`)
         .then(res => res.json())
         .then(json =>{
             const {clothing_items} = json
-            main_container.innerHTML = ""
+            mcContent.innerHTML = ""
             clothing_items.forEach(renderClothingItem);
         })
 }
 
+//helper method to render one clothing item
 const renderClothingItem = function(itemObject){
     const {brand,clothing_type,color} = itemObject
     const itemHTML = `<div>${clothing_type} - ${brand} - ${color}</div>`
-    main_container.innerHTML += itemHTML
+    mcContent.innerHTML += itemHTML
 }
