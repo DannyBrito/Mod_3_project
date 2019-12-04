3634fe2d95683ab8df72b80302c23f0b7be2e1a3
jastaffen <jack.staffen@gmail.com>
2019-12-04 09:44:23 -0500

    dropdown menu

--- frontend/resources/index.js
+++ frontend/resources/index.js
@@ -1,11 +1,12 @@
 const user_container = document.getElementById('user-container');
 const main_container = document.getElementById('main-container');
 const weather_container = document.getElementById('weather-container');
+
 // const allUsers = new InterntalApi(usersUrl);
 
 
 //________________________________________________Jack____________________________________________
-
+let currentUser;
 
 
 
